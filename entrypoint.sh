f53078b0a06de05a8752d526db84f4bc4f2b861d
Kota Inamori <freude1994@gmail.com>
2021-10-12 16:58:59 +0900

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-t
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-if [ ! -z "$INPUT_DELETE_DESTINATION_FOLDER"]
+if [ ! -z "$INPUT_DELETE_DESTINATION_FOLDER" ]
 then
   rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_DELETE_DESTINATION_FOLDER
 fi
